cb5b0c060ccd90a2a752afd86385fcf581c5d82f
Sebastian Gavarini <sebastian.gavarini@natyo.com>
2013-05-03 17:46:22 -0300

    runnable class to test scripts

---
+++ src/test/java/org/elasticsearch/river/rabbitmq/RabbitMQRiverScriptTest.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to ElasticSearch and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.river.rabbitmq;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+import org.elasticsearch.river.rabbitmq.script.MockScriptFactory;
+
+/**
+ *
+ */
+public class RabbitMQRiverScriptTest {
+
+    public static void main(String[] args) throws Exception {
+        Settings settings = ImmutableSettings.settingsBuilder()
+          .put("gateway.type", "none")
+          .put("index.number_of_shards", 1)
+          .put("index.number_of_replicas", 0)
+          .put("script.native.mock_script.type", MockScriptFactory.class)
+          .build();
+        Node node = NodeBuilder.nodeBuilder().settings(settings).node();
+
+        node.client().prepareIndex("_river", "test1", "_meta").setSource(
+            jsonBuilder().startObject()
+              .field("type", "rabbitmq")
+              .startObject("script_filter")
+                .field("script", "mock_script")
+                .field("script_lang", "native")
+            .endObject()).execute().actionGet();
+
+        ConnectionFactory cfconn = new ConnectionFactory();
+        cfconn.setHost("localhost");
+        cfconn.setPort(AMQP.PROTOCOL.PORT);
+        Connection conn = cfconn.newConnection();
+
+        Channel ch = conn.createChannel();
+        ch.exchangeDeclare("elasticsearch", "direct", true);
+        ch.queueDeclare("elasticsearch", true, false, false, null);
+
+        String message =
+        		    "{ \"index\" :  { \"_index\" : \"test\", \"_type\" : \"type1\", \"_id\" : \"1\" } }\n" +
+                "{ \"type1\" :  { \"field1\" : \"value1\" } }\n" +
+                "{ \"delete\" : { \"_index\" : \"test\", \"_type\" : \"type1\", \"_id\" : \"2\" } }\n" +
+                "{ \"create\" : { \"_index\" : \"test\", \"_type\" : \"type1\", \"_id\" : \"3\" } }\n" +
+                "{ \"type1\" :  { \"field3\" : \"value3\" } }" +
+                "";
+
+        ch.basicPublish("elasticsearch", "elasticsearch", null, message.getBytes());
+
+        ch.close();
+        conn.close();
+
+        Thread.sleep(100000);
+    }
+}
---
+++ src/test/java/org/elasticsearch/river/rabbitmq/script/MockScript.java
@@ -0,0 +1,67 @@
+package org.elasticsearch.river.rabbitmq.script;
+
+import org.elasticsearch.common.jackson.core.JsonFactory;
+import org.elasticsearch.common.xcontent.json.JsonXContentParser;
+import org.elasticsearch.script.AbstractExecutableScript;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Map;
+
+public class MockScript extends AbstractExecutableScript {
+  
+  private final Map<String, Object> params;
+  
+  public MockScript(Map<String, Object> params) {
+    super();
+    this.params = params;
+  }
+  
+  @Override
+  public void setNextVar(String name, Object value) {
+    params.put(name, value);
+  }
+  
+  @Override
+  public Object run() {
+    String body = (String) params.get("body");
+    BufferedReader reader = new BufferedReader(new StringReader(body));
+    
+    CharArrayWriter charArrayWriter = new CharArrayWriter();
+    BufferedWriter writer = new BufferedWriter(charArrayWriter);
+    
+    try {
+      process(reader, writer);
+    } catch (IOException e) {
+      // TODO: wrap or treat it
+      throw new RuntimeException(e);
+    }
+    
+    String outputBody = charArrayWriter.toString();
+    System.out.println("input message:\n" + body);
+    System.out.println("output message:\n" + outputBody);
+    
+    return outputBody;
+  }
+
+  private void process(BufferedReader reader, BufferedWriter writer) throws IOException {
+    JsonFactory factory = new JsonFactory();
+    for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+      JsonXContentParser parser = new JsonXContentParser(factory.createParser(line));
+      Map<String, Object> asMap = parser.map();
+      
+      if (asMap.get("create") != null) {
+        // skip "create" operations, header and body
+        reader.readLine();
+      } else {
+        writer.write(line);
+        writer.newLine();
+      }
+    }
+    writer.flush();
+    writer.close();
+  }
+}
