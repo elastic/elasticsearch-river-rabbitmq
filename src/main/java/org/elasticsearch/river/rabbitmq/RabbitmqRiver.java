de31ef8b66770aea091488ff51852bb6b71b0e4d
David Pilato <david@pilato.fr>
2015-04-25 16:58:10 +0200

    Bulk: remove unsafe option
    Due to https://github.com/elastic/elasticsearch/pull/10360
    
    Need to be backported in master (2.0.0) and in es-1.x (1.6.0)
    
    Closes #98.

--- src/main/java/org/elasticsearch/river/rabbitmq/RabbitmqRiver.java
+++ src/main/java/org/elasticsearch/river/rabbitmq/RabbitmqRiver.java
@@ -495,7 +495,7 @@ public class RabbitmqRiver extends AbstractRiverComponent implements River {
             if (script != null) {
                 processBodyPerLine(body, bulkRequestBuilder);
             } else {
-                bulkRequestBuilder.add(body, 0, body.length, false);
+                bulkRequestBuilder.add(body, 0, body.length);
             }
         }
 
@@ -510,7 +510,7 @@ public class RabbitmqRiver extends AbstractRiverComponent implements River {
                 if (asMap.get("delete") != null) {
                     // We don't touch deleteRequests
                     String newContent = line + "\n";
-                    bulkRequestBuilder.add(newContent.getBytes(), 0, newContent.getBytes().length, false);
+                    bulkRequestBuilder.add(newContent.getBytes(), 0, newContent.getBytes().length);
                 } else {
                     // But we send other requests to the script Engine in ctx field
                     Map<String, Object> ctx;
@@ -559,7 +559,7 @@ public class RabbitmqRiver extends AbstractRiverComponent implements River {
                             logger.trace("new bulk request is now: {}", request.toString());
                         }
                         byte[] binRequest = request.toString().getBytes();
-                        bulkRequestBuilder.add(binRequest, 0, binRequest.length, false);
+                        bulkRequestBuilder.add(binRequest, 0, binRequest.length);
                     }
                 }
             }
